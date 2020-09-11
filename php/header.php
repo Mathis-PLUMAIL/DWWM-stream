1c12c4ee03581197d88ae7f76cbc818bdd3d5c98
Mathis-PLUMAIL <mathis.plumail@gmail.com>
2020-09-11 16:21:02 +0200

    legal notice

--- php/header.php
+++ php/header.php
@@ -14,3 +14,38 @@
     <link href="https://fonts.googleapis.com/css2?family=Luckiest+Guy&display=swap" rel="stylesheet">
 </head>
 <body>
+    <h1 class="text-center  p-3"><i class="fas fa-film"></i> DWWM Stream</h1>
+    <nav class="navbar navbar-expand-lg navbar-light navbar-dark bg-dark container">
+        <a class="navbar-brand" href="#">Accueil</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Films <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Series</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Catégories
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Comédie</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Thriller</a>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Actus</a>
+                </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Spider-man" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Rechercher</button>
+            </form>
+        </div>
+    </nav>
---
+++ php/mentions-legales.php
@@ -0,0 +1,5 @@
+<?php require "header.php"; ?>
+
+<h1 class="text-center my-5">Mentions Légales</h1>
+<p class="text-center my-5">Ici les informations conncerant les mentions légales !</p>
+<?php require "footer.php"; ?>
